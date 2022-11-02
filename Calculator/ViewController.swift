c1c6141928de0e37bf5cb23598b9e1c915f118c7
cvios2384 <chdev5102@gmail.com>
2022-11-01 19:37:32 -0700

    Got most of the functionality, just needs +/- & %

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController {
     // values input to the calculator
     // ** Note: Should have used stack here
     // No built-in stack in swift
-    var lstNumbers:[String] = [""]
+    var lstNumbers:[String] = ["",""]
     
     // this holds the index in lstNumbers
     //  that is currently being displayed
@@ -84,6 +84,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayIndex = 0
     }
 
     /*
@@ -96,47 +97,81 @@ class ViewController: UIViewController {
      */
     @IBAction func divisionTapped(_ sender: Any) {
         operation = Operation.division
-        
+        nextInput()
     }
     
     @IBAction func multiplicationTapped(_ sender: Any) {
+        operation = Operation.multiplication
+        nextInput()
     }
     
     @IBAction func subtractionTapped(_ sender: Any) {
+        operation = Operation.subtraction
+        nextInput()
     }
     
     @IBAction func additionTapped(_ sender: Any) {
+        operation = Operation.addition
+        nextInput()
     }
     
-    /* function called when an operation button is
-     tapped , sets up for the second input
-     */
-    
-    func reset(){
-        // advance the index
+    func nextInput(){
+        // advance index
         displayIndex = 1
-        // ensure there is nothing in the
-        // list for second operand
-        lstNumbers[1] = ""
+        
     }
     
     // performs the operation
     // displays the result
     @IBAction func equalsTapped(_ sender: Any) {
         
-        if operation == nil{
+        // don't do anything if
+        // the user hasn't selected an
+        // operation , or the pressed
+        // equals with only 1 operand
+        if operation == nil || lstNumbers[1] == ""{
             return
         }
-        
+        // Now we perform the operation
+        // based on the operator
+        // left number ( first input)
+        let l = Double(lstNumbers[0])
+        // right number ( second input )
+        let r = Double(lstNumbers[1])
+        var res:Double? = nil
         switch operation{
         case .addition:
-            
+            res = l! + r!
         case .subtraction:
+            res = l! - r!
         case .multiplication:
+            res = l! * r!
         case .division:
-            
+            res = l! / r!
+        default:
+            print("fell thru switch")
             
         }
+        
+        // now we apply the result to lstNumbers
+        lstNumbers[0] = String(res!)
+        // reset everything
+        reset()
+        
+        // update display
+        updateDisplay()
+        // now we ask for next input
+        nextInput()
+        
+    }
+    
+    func reset(){
+        // reset displayIndex
+        displayIndex = 0
+        // remove second number
+        lstNumbers[1] = ""
+        // reset the operation
+            operation = nil
         
     }
     /*
@@ -164,6 +199,9 @@ class ViewController: UIViewController {
         updateNumber("7")
     }
     
+    @IBAction func dotPressed(_ sender: Any) {
+        updateNumber(".")
+    }
     
     @IBAction func sixTapped(_ sender: Any) {
         updateNumber("6")
@@ -205,6 +243,16 @@ class ViewController: UIViewController {
         
     }
     
+    // clear nubmer list and operation
+    @IBAction func allClearTapped(_ sender: Any) {
+        displayIndex = 0
+        lstNumbers[0] = ""
+        lstNumbers[1] = ""
+        operation = nil
+        // show the user
+        lblOutput.text = "0"
+        
+    }
     
     
 }
